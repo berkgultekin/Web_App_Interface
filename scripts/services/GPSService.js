c7fc069edcac13259ca093c2ba3fdd304ecbf8d7
tugcaeker <tugcaeker@gmail.com>
2016-11-30 03:14:13 +0300

    Wrong spacings fixed.

--- scripts/services/GPSService.js
+++ scripts/services/GPSService.js
@@ -1,12 +1,12 @@
 app.factory('GPSService', function (API, $http, $filter, $rootScope) {
-	var gps = {
-		list: function () {
-			return $http.get(API.url + 'gpslog/list');
-		},
-		listMission: function (mission_id) {
-			return $http.get(API.url + 'gpslog/list/' + mission_id);
-		},
-		add: function (gpsLog) {
+    var gps = {
+        list: function () {
+            return $http.get(API.url + 'gpslog/list');
+        },
+        listMission: function (mission_id) {
+            return $http.get(API.url + 'gpslog/list/' + mission_id);
+        },
+        add: function (gpsLog) {
             return $http.post(API.url + 'gpslog/create', gpsLog);
         },
         get: function (mission_id) {
@@ -22,14 +22,14 @@ app.factory('GPSService', function (API, $http, $filter, $rootScope) {
             return $http.post(API.url + 'gpslog/create-multi/', gpsLogs);
         },
         getGroup: function (group_key) {
-            return $http.get(API.url + 'gpslog/get-group/'+group_key);
+            return $http.get(API.url + 'gpslog/get-group/' + group_key);
         },
         getLogsByType: function (mission_id, type_id) {
-            return $http.get(API.url + 'gpslog/list/'+mission_id+'/' + type_id);
+            return $http.get(API.url + 'gpslog/list/' + mission_id + '/' + type_id);
         },
-		types: function(){
+        types: function () {
             return $http.get(API.url + 'gpslog/types');
-		}
-	};
-	return gps;
+        }
+    };
+    return gps;
 });
