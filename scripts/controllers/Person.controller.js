8ac8d7c7140e648e6a480570d1a42251306a9cc0
Berk Gültekin <berk.gultekinn@gmail.com>
2016-11-30 06:30:17 +0300

    CRLF test

--- scripts/controllers/Person.controller.js
+++ scripts/controllers/Person.controller.js
@@ -1,45 +1,46 @@
 (function () {
-	'use strict';
+    'use strict';
 
-	angular.module('SearchRescueApp')
-		.config(stateConfig)
-		.controller('PersonController', PersonController);
+    angular.module('SearchRescueApp')
+        .config(stateConfig)
+        .controller('PersonController', PersonController);
 
-	function stateConfig($stateProvider) {
-		$stateProvider.state('person', {
-			url: "/person",
-			controller: 'PersonController',
-			templateUrl: "views/person/main.index.html",
-			controllerAs: 'vm',
-			data: {
-				sideMenu: {
-					text: 'People',
-					styleClass: 'fa fa-users'
-				}
-			}
-		});
-		$stateProvider.state('person.new', {
-			url: "/new",
-			controller: 'PersonController',
-			templateUrl: "views/person/new.index.html",
-			controllerAs: 'vm',
-			data: {
-				sideMenu: {
-					text: 'Add New Person',
-				}
-			}
-		});
-		$stateProvider.state('person.list', {
-			url: "/list",
-			controller: 'PersonController',
-			templateUrl: "views/person/list.index.html",
-			controllerAs: 'vm',
-			data: {
-				sideMenu: {
-					text: 'List People',
-				}
-			}
-		});
+    function stateConfig($stateProvider) {
+        $stateProvider.state('person', {
+            url: "/person",
+            controller: 'PersonController',
+            templateUrl: "views/person/main.index.html",
+            controllerAs: 'vm',
+            data: {
+                sideMenu: {
+                    text: 'People',
+                    styleClass: 'fa fa-users'
+                }
+            }
+        });
+        $stateProvider.state('person.new', {
+            url: "/new",
+            controller: 'PersonController',
+            templateUrl: "views/person/new.index.html",
+            controllerAs: 'vm',
+            data: {
+                sideMenu: {
+                    text: 'Add New Person',
+                }
+            }
+        });
+        $stateProvider.state('person.list', {
+            url: "/list",
+            controller: 'PersonController',
+            templateUrl: "views/person/list.index.html",
+            controllerAs: 'vm',
+            data: {
+                sideMenu: {
+                    text: 'List People',
+                }
+            }
+        });
+
         $stateProvider.state('person.update', {
             url: "/update/:personId",
             controller: 'PersonController',
@@ -52,41 +53,43 @@
                 missionId: null
             }
         });
-	}
+    }
 
 
-	function PersonController($scope, PersonService, $stateParams) {
-
-		$scope.initList = function () {
-			$scope.personList = [];
-			PersonService.getPeople().then(function (response) {
-				console.log(response.data);
-				$scope.personList = response.data;
-			});
-		}
-
-		$scope.initUpdate = function () {
-			$scope.newPerson ={};
-            PersonService.getMission($stateParams.personId).then(function (response) {
+    function PersonController($scope, PersonService, $stateParams) {
+		/* init add page */
+        $scope.initList = function () {
+            $scope.personList = [];
+            PersonService.getPeople().then(function (response) {
+                console.log(response.data);
+                $scope.personList = response.data;
+            });
+        }
+		/* init update page */
+        $scope.initUpdate = function () {
+            $scope.newPerson ={};
+            PersonService.getPerson($stateParams.personId).then(function (response) {
                 console.log(response);
                 $scope.newPerson = response.data;
             });
         }
-		/* init add page */
+
 		/* add new person */
-		$scope.add = function () {
+        $scope.add = function () {
 
-			PersonService.addNewPerson($scope.newPerson).then(function (response) {
-				console.log(response);
-			});
+            PersonService.addNewPerson($scope.newPerson).then(function (response) {
+                console.log(response);
+            });
 
-		}
+        }
+
+		/* update person */
         $scope.update = function () {
-			var filtered = {
-				name : $scope.newPerson.name,
-				surname : $scope.newPerson.surname,
+            var filtered = {
+                name : $scope.newPerson.name,
+                surname : $scope.newPerson.surname,
                 blood_group : $scope.newPerson.blood_group
-			};
+            };
 
             PersonService.updatePerson(filtered, $scope.newPerson.id).then(function (response) {
                 console.log(response);
@@ -94,7 +97,7 @@
 
         }
 
-	}
+    }
 
 
 })();
